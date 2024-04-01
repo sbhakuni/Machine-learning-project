6159978db5e017cfe0b796de1b7aa4b346f36d68
sbhakuni <bhakunishaantanu@gmail.com>
2024-04-01 12:17:47 -0700

    experiment tracker, local deployment

---
+++ README.md
@@ -0,0 +1,18 @@
+```bash
+pip install zenml["server"]
+zenml up
+```
+If you are running the `run_deployment.py` script, you will also need to install some integrations using ZenML:
+
+```bash
+zenml integration install mlflow -y
+```
+
+The project can only be executed with a ZenML stack that has an MLflow experiment tracker and model deployer as a component. Configuring a new stack with the two components are as follows:
+
+```bash
+zenml integration install mlflow -y
+zenml experiment-tracker register mlflow_tracker --flavor=mlflow
+zenml model-deployer register mlflow --flavor=mlflow
+zenml stack register mlflow_stack -a default -o default -d mlflow -e mlflow_tracker --se
+```
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,13 +1,18 @@
 import logging
 from typing import Tuple
+
 import pandas as pd
 from sklearn.base import RegressorMixin
 from typing_extensions import Annotated
 from zenml import step
 
 from src.evaluation import MSE, R2, RMSE
+import mlflow
+from zenml.client import Client
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
     model: RegressorMixin,
     X_test: pd.DataFrame,
@@ -25,13 +30,13 @@ def evaluate_model(
         prediction = model.predict(X_test)
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test, prediction)
-
+        mlflow.log_metric("mse", mse)
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test, prediction)
-
+        mlflow.log_metric("r2", r2)
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test, prediction)
-
+        mlflow.log_metric("rmse", rmse)
         return r2, rmse
     except Exception as e:
         logging.error("Error in evaluating model: {}".format(e))
